c8878475d08f6e34ca2aad8372fa28af41b35832
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2023-09-25 19:38:09 -0700

    task1x1B

---
+++ deck.h
@@ -0,0 +1,49 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdlib.h>
+
+/**
+ * enum kind_e - Enumeration
+ * @SPADE: Spade
+ * @HEART: Heart
+ * @CLUB: Club
+ * @DIAMOND: Diamond
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - card
+ *
+ * @value: Value
+ * @kind: Kind
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Point to card node
+ * @prev: Point to previous node
+ * @next: Point to next node
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
+void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_H */
